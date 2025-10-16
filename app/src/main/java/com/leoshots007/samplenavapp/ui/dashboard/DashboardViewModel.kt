3a13d47ac1ff0fa7810198e4d4614aadd70ad2ef
dudsaheb <dudsaheb@gmail.com>
2025-10-16 19:22:07 +0530

    first commit

---
+++ app/src/main/java/com/leoshots007/samplenavapp/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.leoshots007.samplenavapp.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
